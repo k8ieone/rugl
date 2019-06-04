75e851aa353a9d92697f21788e0c0ad6d62aff8d
Jiří Sedlák <jiri.s.15243@gmail.com>
2019-06-04 17:38:14 +0200

    v0.0.3

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((400, 300))
+clock = pygame.time.Clock()
 game = True
 x = 10
 y = 10
@@ -19,6 +20,8 @@ while game:
         if pressed[pygame.K_LEFT]: x -= 10
         if pressed[pygame.K_RIGHT]: x += 10
 
+    screen.fill((0, 0, 0))
     pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(x, y, 60, 60))
 
     pygame.display.flip()
+    clock.tick(60)

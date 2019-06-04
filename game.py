2bd7d05fe1b2a01c7c6fa5046165559f8a48a61d
Jiří Sedlák <jiri.s.15243@gmail.com>
2019-06-04 18:12:01 +0200

    v0.1.1

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 pygame.init()
 screen = pygame.display.set_mode((400, 300))
@@ -6,6 +7,17 @@ clock = pygame.time.Clock()
 game = True
 x = 10
 y = 10
+
+_image_library = {}
+def get_image(path):
+    global _image_library
+    image = _image_library.get(path)
+
+    if image == None:
+        canonicalized_path = path.replace("/", os.sep).replace("\\", os.sep)
+        image = pygame.image.load(canonicalized_path)
+        _image_library[path] = image = pygame.transform.scale(image, (70, 50))
+    return image
 
 while game:
 
@@ -21,7 +33,7 @@ while game:
         if pressed[pygame.K_RIGHT]: x += 10
 
     screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(x, y, 60, 60))
+    screen.blit(get_image("D:\\Users\\Gentle\\Desktop\\rock.png"), (x, y))
 
     pygame.display.flip()
     clock.tick(60)

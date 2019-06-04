d93ebac6a672eda08c4b12fc38123339f0adb7d9
Jiří Sedlák <jiri.s.15243@gmail.com>
2019-06-04 17:25:11 +0200

    v0.0.1

--- game.py
+++ game.py
@@ -1,1 +1,17 @@
-while True:
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+game = True
+
+while game:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+                game = False
+        else:
+            print(event.type)
+
+    pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(30, 30, 60, 60))
+
+    pygame.display.flip()

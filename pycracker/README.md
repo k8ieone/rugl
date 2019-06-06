bbeb7e645ef60cfac512472eb120cd4ce119d436
satcom886 <satcom886@Zinc.local>
2019-06-06 18:00:41 +0200

    Initial commit. Candidate generation works.

---
+++ pycracker/README.md
@@ -0,0 +1,4 @@
+# Where are we going with this?
+This is a project that aims to create the worlds slowest password cracker. Feel free to hate. My memory management is a mess right now so this program is quite good at eating up RAM. My warning: Do not run it with password length lagrer than 4.
+# Why is it consuming so much memory?
+I have all the password candidates stored in a single dictionary variable. Messy but works.
---
+++ pycracker/pycracker.py
@@ -0,0 +1,35 @@
+# Usage: pycracker.py HASH CHARACTERS HASHTYPE
+
+import hashlib
+import sys
+import itertools
+
+def gen_list_of_potential_passwords(characters, length):
+    #if "a" in sys.argv[-2] and "A" not in sys.argv[-2] and "1" not in sys.argv[-2]:
+    if "a" in characters and "A" not in characters and "1" not in characters:
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+    
+    elif "a" in characters and "A" in characters and "1" not in characters:
+        alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    
+    elif "a" in characters and "A" in characters and "1" in characters:
+        alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    
+    elif "a" not in characters and "A" in characters and "1" in characters:
+        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    
+    elif "a" not in characters and "A" not in characters and "1" in characters:
+        alphabet = "0123456789"
+    
+    elif "a" in characters and "A" not in characters and "1" in characters:
+        alphabet = "abcdefghijklmnopqrstuvwxyz0123456789"
+    
+    elif "a" not in characters and "A" in characters and "1" not in characters:
+        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    
+    print("Generating possible password candidates...")
+    dictionaryofcandidates = {''.join(p) for p in itertools.permutations(alphabet, length)}
+    print("Done!")
+    return dictionaryofcandidates
+
+# print(gen_list_of_potential_passwords("aA1", 3))

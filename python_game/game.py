cdcb77fa22b544508af08141d52c15f543190600
Jiří Sedlák <jiri.s.15243@gmail.com>
2019-06-06 21:58:54 +0200

    v0.2.0

--- python_game/game.py
+++ python_game/game.py
@@ -5,16 +5,19 @@ import time
 
 # some basic shit
 pygame.init()
-screen = pygame.display.set_mode((400, 300))
+width = 800
+heigh = 800
+screen = pygame.display.set_mode((width, heigh))
 game = True
 x = 10
 y = 10
-counter = 0
 image__path = os.path.dirname(os.path.abspath(__file__)) + "/Resources/rock.png"
 number_of_obstacles_on_screen = 5
 obstacles = 0
 list_of_obstacles = []
-
+speed1 = 2
+speed2 = 5
+past = time.clock()
 
 # class of the obstacle (obviously) that will be falling down
 class Obstacle:
@@ -30,22 +33,27 @@ class Obstacle:
     def __repr__(self):
         return "x: %s, y: %s, z: %s, speed: %s" % (self.x, self.y, self.z, self.speed)
 
-for i in range(number_of_obstacles_on_screen):
-            try:
-                if obstacles[i] == None:
-                    z = random.randint(70, 170)
-                    speed = random.randint(5, 50)
-                    name = Obstacle((i * z), 0, z, speed)
-                    list_of_obstacles.append(name)
+def obstacleCreator(speed1, speed2):
 
-            except:
-                z = random.randint(70, 170)
-                speed = random.randint(5, 50)
-                name = Obstacle((i * z), 0, z, speed)
-                list_of_obstacles.append(name)
+    global list_of_obstacles
+    global number_of_obstacles_on_screen
+
+    for i in range(number_of_obstacles_on_screen):
+        
+        try:
+            dummy = list_of_obstacles[i]
+
+        except:
+            z = random.randint(50, 100)
+            x = random.randint(0, width)
+            speed = random.randint(speed1, speed2)
+            dummy = Obstacle(x, 0, z, speed)
+            list_of_obstacles.append(dummy)
+
+obstacleCreator(speed1, speed2)
 
 # actual fuckin' game
-while game:
+while game: 
 
     # you know. game should close when you hit the cross thingy
     for event in pygame.event.get():
@@ -55,37 +63,37 @@ while game:
         # controls for arrow keys (this will be changed soon)
         pressed = pygame.key.get_pressed()
 
-        if pressed[pygame.K_UP]: y -= 10
-        if pressed[pygame.K_DOWN]: y += 10
-        if pressed[pygame.K_LEFT]: x -= 10
-        if pressed[pygame.K_RIGHT]: x += 10
+        if pressed[pygame.K_UP]:
+            y -= 10
+        if pressed[pygame.K_DOWN]: 
+            y += 10
+        if pressed[pygame.K_LEFT]: 
+            x -= 10
+        if pressed[pygame.K_RIGHT]:
+            x += 10
 
     #screen shit
     screen.fill((0, 0, 0))
     
     #creating list of obstacles
-    if counter // 108000 == 0: # 1 minute = 216, 000 (maybe)
+    now = time.clock()
+    if int(now) == int((past + 30)): # 1 minute = 3600
         number_of_obstacles_on_screen += 1
-
-        for i in range(number_of_obstacles_on_screen):
-            try:
-                if obstacles[i] == None:
-                    z = random.randint(70, 170)
-                    speed = random.randint(5, 50)
-                    name = Obstacle((i * z), 0, z, speed)
-                    list_of_obstacles.append(name)
-            except:
-                z = random.randint(70, 170)
-                speed = random.randint(5, 50)
-                name = Obstacle((i * z), 0, z, speed)
-                list_of_obstacles.append(name)
+        speed1 += 1
+        speed2 += 2
+        obstacleCreator(speed1, speed2)
+        past = time.clock()
     
     #getting obstacles to the screen
     for i in range(len(list_of_obstacles)):
         screen.blit(list_of_obstacles[i].image, (list_of_obstacles[i].x, list_of_obstacles[i].y + list_of_obstacles[i].speed))
         list_of_obstacles[i].y = list_of_obstacles[i].y + list_of_obstacles[i].speed
+        if list_of_obstacles[i].y > heigh:
+            list_of_obstacles.pop(i)
+            obstacleCreator(speed1, speed2)
 
     #end shit
-    counter += 1
     pygame.display.flip()
     pygame.time.Clock().tick(60)
+    
+

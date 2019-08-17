028a65d577fbb062c6136ac9bdb9a2a7a868c429
satcom886 <rostik.medved@gmail.com>
2019-08-17 18:29:50 +0200

    Colors and code clean up

--- bash/arch-setuptools/00_base.sh
+++ bash/arch-setuptools/00_base.sh
@@ -8,19 +8,18 @@
 # set -e # This auto exits the script if any command returns a non-zero value
 
 # COLORS
-#red=`tput setaf 1`
-#green=`tput setaf 2`
-#reset=`tput sgr0`
-# maybe later...
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
 
 # Check internet access
 
 wget -q --spider https://archlinux.org
 if [ $? -eq 0 ]
 then
-    echo "Network OK"
+    :
 else
-    echo "Internet connection not available"
+    echo "${red}Internet connection not available"
     echo "Aborting..."
     exit 1
 fi
@@ -30,10 +29,9 @@ fi
 fdisk -l /dev/$1
 if [ $? -eq 0 ]
 then
-    echo "Partition $1 is a valid partition"
-    echo
+    :
 else
-    echo "Partition $1 not found"
+    echo "${red}Partition $1 not found"
     echo "Aborting..."
     exit 1
 fi
@@ -46,8 +44,6 @@ then
     echo -n "Please specify the EFI system partition:"
     read _EFIPART
 else
-    echo You are running a traditional BIOS
-    echo
     _BOOTMODE=BIOS
 fi
 
@@ -56,13 +52,14 @@ fi
 echo Let\'s set the correct time
 timedatectl set-ntp true
 
-# Partition
+# Format and mount root
+echo "${red}WARNING"
 echo "$1 will be formated as ext4 and used as root"
 echo "Is this OK?"
 read _OK
 if  [[ $_OK == y* ]]
 then
-    echo "Moving on"
+    :
 elif [[ $_OK == n* ]]
 then
     echo "Aborted by user"
@@ -76,8 +73,11 @@ mkfs.ext4 /dev/$1
 sleep 2
 mount /dev/$1 /mnt
 
+# Here we format and mount the EFI partition
+
 if [ $_BOOTMODE == EFI]
 then
+    echo "${red}WARNING"
     echo "$_EFIPART will be formated as FAT32"
     echo "If you answer no the partition will be left untouched"
     read _OK
@@ -97,8 +97,6 @@ then
     fi
 fi
 
-# This is where we would format and mount the EFI partition
-
 # Installing the base packages
 pacstrap /mnt base
 
@@ -107,12 +105,15 @@ genfstab -U /mnt >> /mnt/etc/fstab
 cat /mnt/etc/fstab
 
 echo
-echo "Installation finished"
+echo "${green}Installation finished"
 echo "Brief summary of what was done:"
 echo "1. Formated root as ext4"
 if [[ $_OK == y* ]]
 then
     echo "1.5. Formated EFI system partition as FAT32"
+else
+    :
+fi
 echo "2. installed the base package group to root"
 echo "3. Generated a fstab"
 echo

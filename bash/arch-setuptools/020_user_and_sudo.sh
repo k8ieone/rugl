6b8f2f3934e64822ac5e6d67b891812910419498
satcom886 <rostik.medved@gmail.com>
2019-08-18 21:35:11 +0200

    Added future plans

---
+++ bash/arch-setuptools/020_user_and_sudo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This will just create a user and add it to /etc/sudoers
+# Also install sudo and remove this repo from this directory
+# and clone it into the new user's home (make sure the user owns the directory)
+# Maybe I should enable NTP again?
---
+++ bash/arch-setuptools/030_final_tweaks_headless.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This will take you the rest of the way.
+# It will create a swap file, generate a ssh key, install and configure ZSH, 
+# add you to some groups, install libvirt?, hashtopolis?, toilet, neofetch, cron?, netdata, boinc, yay and powerpill (+pacserve)
+# When this script finishes running, the machine will be configured to my standards. 

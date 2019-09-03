574d23cb5773de34669e48601df4a82e182606c2
Rostislav Medvěd <rostik.medved@protonmail.ch>
2019-09-03 18:40:43 +0200

    Hopefully fixed the hardcoded path

--- bash/arch-setuptools/030_final_tweaks_headless.sh
+++ bash/arch-setuptools/030_final_tweaks_headless.sh
@@ -11,9 +11,22 @@ green=$(tput setaf 2)
 reset=$(tput sgr0)
 
 # Ask about SWAP and set it up
+echo "Do you wish to setup a SWAP file?"
+echo -n "y/N: "
+read -r _SWAP
+if [[ $_SWAP == y* ]]
+then
+    sudo fallocate -l 2048M /swapfile
+    sudo chmod 600 /swapfile
+    sudo mkswap /swapfile
+    sudo swapon /swapfile
+    echo "/swapfile none swap defaults 0 0" | sudo tee -a /etc/fstab
+else
+    :
+fi
 
 # Install some packages
-sudo pacman -S boinc-nox zsh make gcc gc automake autoconf pkg-conf fakeroot binutils netdata hddtemp smartmontools lm_sensors neofetch
+sudo pacman -S boinc-nox zsh make gcc gc automake autoconf pkgconf fakeroot binutils netdata hddtemp smartmontools lm_sensors neofetch
 
 echo "Your new SSH private and public key will be generated now..."
 ssh-keygen
@@ -47,15 +60,18 @@ fi
 # ZSH stuff
 echo
 echo "${red}WARNING!${reset} An external script will now be run! Is this OK?"
-echo "y/N: "
+echo -n "y/N: "
 read _OK
 if [[ $_OK == y* ]]
 then
+    echo "${red}WARNING!${reset}"
+    echo "The following script will drop you to ZSH, be sure to type \"exit\" to continue the script."
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-    mv ~/rugl/bash/arch-setuptools/configs/user/zshrc ~/.zshrc
+    echo "export ZSH=\"/home/$USERNAME/.oh-my-zsh\"" > ~/.zshrc
+    cat ~/rugl/bash/arch-setuptools/configs/user/zshrc | tee -a ~/.zshrc
 else
     echo "ZSH setup will be skipped!"
 fi

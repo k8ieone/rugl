d26c4946bc18615e0d77e5f0e02a51f87c8390a4
satcom886 <rostik.medved@gmail.com>
2019-09-20 23:21:18 +0200

    First and very basic version of caesar_cipher

--- python3/caesar_cipher/caesar_cipher_satcom886.py
+++ python3/caesar_cipher/caesar_cipher_satcom886.py
@@ -1,25 +1,31 @@
-#!/bin/bash
+#!/usr/bin/python
 # The teacher wouldn't let us finish this in class, so I ignored her solution and did it my way.
 # Also she kept me from focusing by always talking! So this is not the finished version...
 # -------------------------------------------------------------------------------------------------
 # Notes for myself: 
 # Subtract 26!
-# Use ord and chr
 # Also try to do it without ord and chr
 
 import string
 import random
 
-def choose_offset():
-    offset = random.randrange(1,5)
-    return offset
-
-def encode(string, offset):
-    ;string_numbers = []
+def encode(string):
+    offset = random.randrange(1, 5)
+    string_in_numbers = []
+    string_encoded = ""
     for i in range(len(string)):
-        string_numbers.append(ord(string[i]))
-        string_numbers[i] = string_numbers[i + offset]
-        new_string = new_string + chr(string_numbers[i])
-    return new_string + offset
+        string_in_numbers.append(ord(string[i]))
+        string_encoded += chr(string_in_numbers[i] + offset)
+    return [string_encoded, offset]
 
-print(encode("test", 2))
+print(encode("test"))
+
+def decode(string_encoded, offset):
+    string_in_numbers = []
+    string_decoded = ""
+    for i in range(len(string_encoded)):
+        string_in_numbers.append(ord(string_encoded[i]))
+        string_decoded += chr(string_in_numbers[i] - offset)
+    return string_decoded
+
+print(decode("uftu", 1))

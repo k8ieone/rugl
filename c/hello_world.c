f4788e8711e5497e2af653d7a9a99eb3d6c1d49d
satcom886 <rostik.medved@gmail.com>
2019-09-26 15:58:06 +0200

    Hello World!

---
+++ c/hello_world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    // char a[20];
+    printf("Hello world!\n");
+    // printf("Enter a string: ");
+    // scanf(a);
+    // printf("\nYou enterd \"%s\"", a);
+    return 0;
+}

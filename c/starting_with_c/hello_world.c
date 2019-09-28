508891fd82968a7e9f23eb4a32f033e0d1256054
satcom886 <rostik.medved@gmail.com>
2019-09-28 16:54:24 +0200

    Removed the comments

--- c/starting_with_c/hello_world.c
+++ c/starting_with_c/hello_world.c
@@ -2,10 +2,6 @@
 #include <stdlib.h>
 
 int main(void) {
-    // char a[20];
     printf("Hello world!\n");
-    // printf("Enter a string: ");
-    // scanf(a);
-    // printf("\nYou enterd \"%s\"", a);
     return 0;
 }

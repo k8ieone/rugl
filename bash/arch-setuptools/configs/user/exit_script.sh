1013d87c05ce4a4ef39b6b1db421b3578084c07f
satcom886 <satcom886@Zinc.local>
2019-10-13 23:51:26 +0200

    Nah, I think I got it...

--- bash/arch-setuptools/configs/user/exit_script.sh
+++ bash/arch-setuptools/configs/user/exit_script.sh
@@ -8,14 +8,14 @@ if [ $? -eq 0 ]
 then
     if [[ $act=="logout" ]]
     then
-        #killall nm-applet
-        #killall redshift-gtk
-        #killall /usr/bin/python2 /usr/bin/indicator-cpufreq
-        #killall /usr/lib/notification-daemon-1.0/notification-daemon
-        #killall mpd
-        #killall blueman-applet
+        killall nm-applet
+        killall redshift-gtk
+        killall /usr/bin/python2 /usr/bin/indicator-cpufreq
+        killall /usr/lib/notification-daemon-1.0/notification-daemon
+        killall mpd
+        killall blueman-applet
         i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -B 'Yes, exit i3' 'i3-msg exit'
-        pkill -u `id -u $USER`
+        #pkill -u `id -u $USER`
     else
         systemctl $act -i
     fi

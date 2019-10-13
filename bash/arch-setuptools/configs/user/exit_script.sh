1c08f5c9ebc1e9070e899bc7204ffe65c195605a
satcom886 <satcom886@Zinc.local>
2019-10-13 23:38:09 +0200

    Improved the exit script

--- bash/arch-setuptools/configs/user/exit_script.sh
+++ bash/arch-setuptools/configs/user/exit_script.sh
@@ -2,5 +2,22 @@
 act=$(echo "reboot
 poweroff
 hibernate
-suspend" | dmenu) 
-systemctl $act
+suspend
+logout" | dmenu)
+if [ $? -eq 0 ]
+then
+    if [[ $act=="logout" ]]
+    then
+        killall nm-applet
+        killall redshift-gtk
+        killall indicator-cpufreq
+        killall /usr/lib/notification-daemon-1.0/notification-daemon
+        killall mpd
+        killall blueman-applet
+        i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -B 'Yes, exit i3' 'i3-msg exit'
+    else
+        systemctl $act -i
+    fi
+else
+    exit 1
+fi

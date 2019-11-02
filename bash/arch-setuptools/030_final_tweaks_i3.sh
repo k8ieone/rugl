1bceef07ef4f292e321502e039df94e08e3dba27
satcom886 <rostik.medved@gmail.com>
2019-11-02 14:50:59 +0100

    Added caffeine

--- bash/arch-setuptools/030_final_tweaks_i3.sh
+++ bash/arch-setuptools/030_final_tweaks_i3.sh
@@ -35,7 +35,7 @@ sudo rm -r yay
 
 # Install Xorg, i3 and some stuff
 sudo pacman -S xorg dmenu xsecurelock i3-gaps notification-daemon mpc i3status xorg-xinit glava ttf-font-awesome adapta-gtk-theme mpd scrot redshift powerline-fonts network-manager-applet bluez bluez-utils blueman terminator ttf-ubuntu-font-family pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-zeroconf paprefs pavucontrol arandr
-yay -S paper-icon-theme-git i3cat-git indicator-powersave powerpill toilet clight compton-tryone-git
+yay -S paper-icon-theme-git i3cat-git indicator-powersave powerpill toilet clight compton-tryone-git caffeine-ng
 
 ## Configs
 # i3status config

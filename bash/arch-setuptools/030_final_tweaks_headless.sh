2d50d1375029d764a7ddb8bd58734414f2fd4b21
satcom886 <rostik.medved@gmail.com>
2019-12-01 16:19:29 +0100

    I'm retarded

--- bash/arch-setuptools/030_final_tweaks_headless.sh
+++ bash/arch-setuptools/030_final_tweaks_headless.sh
@@ -26,7 +26,7 @@ else
 fi
 
 # Install some packages
-sudo pacman -Sbtrfs device delete /dev/sdc zsh crda nano make gcc gc patch automake autoconf pkgconf fakeroot binutils netdata hddtemp smartmontools lm_sensors neofetch rng-tools opensc systemd-swap
+sudo pacman -S zsh crda nano make gcc gc patch automake autoconf pkgconf fakeroot binutils netdata hddtemp smartmontools lm_sensors neofetch rng-tools opensc systemd-swap
 
 echo "Your new SSH private and public key will be generated now..."
 ssh-keygen

69e79f8b281ffacb2fa2edebe5573ecb3017c6e0
satcom886 <rostik.medved@gmail.com>
2019-12-03 13:26:09 +0100

    OwO

--- school_stuff/useless_programs/pajka_easy.ino
+++ school_stuff/useless_programs/pajka_easy.ino
@@ -1,31 +1,40 @@
+//Vypadá to, že jsi to vzdal...
+//To je v pohodě, můžeš si vypůjčit můj kód...
+//Prosím snaž se kód upravir aby nebylo poznat, že je to jenom zkopírované
+//Například uprav kolik tlačítka ubírají a přidávají
+//Toto nastavení najdeš kousek pod tímto komentářem, je označeno komentářem "tlačítka"
+//To samé jde aplikovat na LED diody
+//Také třeba uprav tóny a délky tónů.
+//Nebo zprávu, která je vypsána do sériové konzole
+//Taky prosím o smazání všech (nebo alespoň většiny) komentářů
+//Děkuji
+
 #include <EEPROM.h>
 
 #define pinLatch 4
 #define pinClk 7
 #define pinData 8
 byte cislice=3;
-// Above is the display magic
 
-//leds
-const int led1 = 13; // heating is on
-const int led2 = 12; // if the heating logic is active
-const int led3 = 11;
-const int led4 = 10;
+//ledky
+const int led1 = 10; // topné těleso
+const int led2 = 11; // zepnuto/vypnuto
+const int led3 = 12; // připraveno k použití
+const int led4 = 13;
 
-//buttons
-const int but1=A1; // -button
+//tlačítka
+const int but1=A3; // -button
 const int but2=A2; // +button
-const int but3=A3; // toggle
+const int but3=A1; // on/off
 
-//other variables
-byte temperature_target;
+byte teplotaNastavena;
 
 void setup() {
-  pinMode(3, OUTPUT);
   pinMode(pinLatch, OUTPUT);
   pinMode(pinClk, OUTPUT);
   pinMode(pinData, OUTPUT);
-  // More magic above
+  
+  pinMode(3, OUTPUT);
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
@@ -33,44 +42,45 @@ void setup() {
   pinMode(but1, INPUT_PULLUP);
   pinMode(but2, INPUT_PULLUP);
   pinMode(but3, INPUT_PULLUP);
+
   Serial.begin(9600);
   digitalWrite(led3, HIGH);
   digitalWrite(led4, HIGH);
+
+  //Načíst hodnotu z EEPROM
   if (EEPROM.read(100) == 1){
-    temperature_target = 100;
+    teplotaNastavena = 100;
   }
   else {
-    temperature_target = EEPROM.read(100);
+    teplotaNastavena = EEPROM.read(100);
   }
-  tone(3, 750, 100);
-  delay(100);
-  tone(3, 1000, 50);
-  delay(75);
-  tone(3, 1000, 50);
-  delay(80);
-  tone(3, 800, 50);
-}
+  //Tón při zapnutí
+  tone(3, 6000, 150);
+  delay(150);
+  tone(3, 7500, 50);
+  }
 
 void loop() {
-  byte temperature_current = 255 - analogRead(A4)/4;
+  byte teplotaAktualni = 255 - analogRead(A4)/4;
   
-  // Printing the temperatures to the serial console
-  Serial.print("Current temperature is: ");
-  Serial.print(temperature_current);
-  Serial.print("  ");
-  Serial.print("Target temperature is: ");
-  Serial.println(temperature_target);
+  //Tady píšeme do sériové konzole
+  Serial.print("Aktuální teplota: "); // PLEASE CHANGE THIS
+  Serial.print(teplotaAktualni);
+  Serial.print("\n");
+  Serial.print("Nastavená teplota: "); // PLEASE CHANGE THIS
+  Serial.print(teplotaNastavena);
+  Serial.print("\n");
   
-  // Display current temperature on the display
-  display_something(temperature_current);
+  //Tady zobrazujeme na displeji
+  zapisCisloNaSegment(teplotaAktualni);
   
-  // Check if the heating should be on (compare target and current temperature)
+  //Tady kontrolujeme aktuální a nastavenou teplotu
   if (digitalRead(led2) == 0){
-    if (temperature_current < temperature_target){
+    if (teplotaAktualni < teplotaNastavena){
       digitalWrite(led1, LOW);
       digitalWrite(led3, LOW);
     }
-    else if (temperature_current > temperature_target){
+    else if (teplotaAktualni > teplotaNastavena){
       digitalWrite(led1, HIGH);
     }
   }
@@ -78,45 +88,27 @@ void loop() {
     digitalWrite(led1, HIGH);
   }
 
-  // Buttons and their logic
-  //-Button
+  // Tlačítka
   if (digitalRead(but1) == 0){
-    temperature_target -= 5;
-    EEPROM.write(100, temperature_target);
+    teplotaNastavena -= 5;
+    EEPROM.write(100, teplotaNastavena);
   }
-  //+Button
   else if (digitalRead(but2) == 0){
-    temperature_target += 10;
-    EEPROM.write(100, temperature_target);
+    teplotaNastavena += 10;
+    EEPROM.write(100, teplotaNastavena);
   }
-  //ON/OFF button
   else if (digitalRead(but3) == 0 and digitalRead(led2) == 0){
     digitalWrite(led2, HIGH);
     digitalWrite(led3, HIGH);
     delay(150);
-    tone(3, 2000, 100);
-    delay(100);
-    tone(3, 1750, 50);
   }
   else if (digitalRead(but3) == 0 and digitalRead(led2) == 1){
     digitalWrite(led2, LOW);
     delay(150);
-    tone(3, 1750, 100);
-    delay(100);
-    tone(3, 2000, 50);
-
-  }
+}
 }
 
-void display_something(int string){
-  int counter = 0;
-  while (counter < 1000){
-    counter += 1;
-    zapisCisloNaSegment(string);
-  }
-}
-
-//Below is even more display magic
+//Magie pro displej
 void zapisCisloNaSegment(int hodnota) {
 const byte mapaSegment[] = {0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0X80,0X90};
  switch (cislice--) {
@@ -147,4 +139,3 @@ digitalWrite(pinLatch,HIGH);
  cislice=3;
  }
 }
-// End of display magic

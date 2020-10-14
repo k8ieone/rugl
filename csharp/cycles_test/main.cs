e312c164f9d5c2a5c73786170c06023c76380cff
satcom886 <rostik.medved@gmail.com>
2020-10-14 11:35:47 +0200

    Just moving to another PC

---
+++ csharp/cycles_test/main.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Globalization;
+namespace Calculator{
+    class Calculator{
+        static void Main(){
+            // Here we get the input
+            Console.Write("Enter the first number: ");
+            string numberOne = Console.ReadLine();
+            Console.Write("Enter the second number: ");
+            string numberTwo = Console.ReadLine();
+        }
+    }
+}

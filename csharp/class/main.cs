ba8cbbaa064c1584863580139e2b275b1fad7e83
satcom886 <rostik.medved@gmail.com>
2020-10-14 18:17:54 +0200

    Not working yet, I have to ask the teacher

---
+++ csharp/class/main.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Globalization;
+namespace ClassesTest{
+    class MainClass{
+        static void Main(){
+            Cat cat1 = new Cat("Victoria");
+            Cat cat2 = new Cat("Ellie");
+            Cat cat3 = new Cat();
+            cat1.sayMeow();
+            cat2.sayMeow();
+            cat3.sayMeow();
+            }
+        }
+    public class Cat{
+        public void sayMeow(){
+            Console.WriteLine(self.catName + ": Meow");
+        }
+        public Cat(){
+            catName = "unnamed";
+        }
+        public Cat(string name){
+            catName = name;
+        }
+        public string Name { get; }
+    }
+}
